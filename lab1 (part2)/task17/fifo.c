283c3fe318a17ed09f1ad3cdc42c651908e69fdf
mansbackstrom <mans.backstrom@hotmail.se>
2025-11-10 16:00:14 +0100

    comments added to part 2

--- lab1 (part2)/task17/fifo.c
+++ lab1 (part2)/task17/fifo.c
@@ -1,4 +1,5 @@
-/*Task 17. Implementation of FIFO page replacement
+/*
+Task 17. Implementation of FIFO page replacement
 Your task is to write a program that calculates the number of page faults for a sequence of memory references (i.e., the
 memory reference trace in the .mem-files) when using the FIFO (First-In-First-Out) page replacement policy.
 Your program shall take as input the number of physical pages, the page size, and the name of the trace file, see the example
---
+++ lab1 (part2)/task20/lru.c
@@ -0,0 +1,7 @@
+/*
+Your task is to write a program that calculates the number of page faults for a sequence of memory references (i.e., the
+memory reference trace in the .mem-files) when using the LRU (Least-Recently-Used) page replacement policy.
+Your program shall take as input the number of physical pages, the page size, and the name of the trace file, see the example
+below:
+./lru no_phys_pages page_size filename
+*/
