ebb760a1255f98de9e20f2067b60a4ce72165560
Liam <lgustafsson71@gmail.com>
2025-11-11 16:06:15 +0100

    task13 and task17

--- lab1 (part2)/task17/fifo.c
+++ lab1 (part2)/task17/fifo.c
@@ -6,3 +6,50 @@ Your program shall take as input the number of physical pages, the page size, an
 below:
 ./fifo no_phys_pages page_size filename
 */
+#include <stdlib.h>
+#include <stdio.h>
+
+int find(int arr[], int n, int value){
+    for(int i =0; i<n; ++i){
+        if(arr[i]==value)
+            return i;
+    }
+    return -1;
+}
+
+int main(int argc, char *argv[]){
+
+    unsigned int page_faults=0;
+    unsigned int no_phys_pages=atoi(argv[1]);
+    unsigned int page_size=atoi(argv[2]);
+    char *filename=argv[3];
+    int arr[no_phys_pages];
+    char line[256];
+    unsigned int page=0;
+    int found=-1;
+    unsigned int next_place= 0;
+    unsigned int int_line= 0;
+    for (int i = 0; i < no_phys_pages; i++) {
+    arr[i] = -1; //so that no accidenatl match occurs
+    }
+
+    FILE *file = fopen(filename, "r");
+    if(file == NULL){
+        printf("Could not open the file!");
+    }
+    else{
+        while (fgets(line, sizeof(line), file)) {
+            int_line = atoi(line);
+            page=int_line/page_size;
+            found=find(arr,no_phys_pages,page);
+            if(found==-1){
+                arr[next_place]= page;
+                next_place=(next_place+1)%no_phys_pages;
+                page_faults++;
+            }
+        }
+        fclose(file);
+    }
+    printf("Total amount of page faults: %d\n",page_faults);
+    
+}
