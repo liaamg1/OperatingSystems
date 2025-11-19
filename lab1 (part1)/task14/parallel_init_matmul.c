18941371f11bef82f2bdb543517c9366d860e951
Liam <lgustafsson71@gmail.com>
2025-11-19 17:10:45 +0100

    a

--- lab1 (part1)/task14/parallel_init_matmul.c
+++ lab1 (part1)/task14/parallel_init_matmul.c
@@ -1,6 +1,9 @@
 /*
 Task 14. Parallelization of the initialization
-Parallelize the initialization of the matrices also.
+Parallelize the initialization of the matrices also, i.e., the function init_matrix. Use one thread to initialize each of the
+rows in the matrices a and b. Compile, link, and execute your program.
+Which is the execution time and speedup for the application now?
+Did the program run faster or slower now, and why?
 */
 
 #include <stdio.h>
